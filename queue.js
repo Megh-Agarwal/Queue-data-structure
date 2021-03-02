d9b8aa97ae1a1a6b7a0e6e6caeb96ac84f7763ee
Megh Agarwal <33357611+Megh-Agarwal@users.noreply.github.com>
2021-03-02 20:39:25 +0530

    Add files via upload

---
+++ queue.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(data) {
+       this.data = data;
+   }
+}
+
+class Queue {
+    constructor() {
+       this.elements = [];
+    }
+    
+    enqueue(node) {
+       this.elements.push(node);
+    }
+  
+    dequeue() {
+       if(this.elements.length > 0) { 
+           return this.elements.shift();
+       } else {
+           return 'Underflow situation';
+       }
+    }
+    isEmpty() {
+       return this.elements.length == 0;
+    }
+    
+    front() {
+       if(this.elements.length > 0) {
+           return this.elements[0];
+       } else {
+           return "The Queue is empty!";
+       }
+    }
+    
+    print() {
+       return this.elements;
+    }
+}
